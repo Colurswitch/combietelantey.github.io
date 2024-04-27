1b21d8b5b0e9fd95526c998b03ceef87d8469ba0
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2024-04-27 16:19:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# combietelantey.github.io
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Colurswitch/combietelantey.github.io)
---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
+import { getPerformance } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-performance.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC4zPm3Y3uZJBqU1t7H_DVmxmhHQ96gi_E',
+  authDomain: 'combiete-lantey.firebaseapp.com',
+  databaseURL: 'https://combiete-lantey-default-rtdb.firebaseio.com',
+  projectId: 'combiete-lantey',
+  storageBucket: 'combiete-lantey.appspot.com',
+  messagingSenderId: '247060132443',
+  appId: '1:247060132443:web:34804218276493e396c57e',
+  measurementId: 'G-X9J01FH514',
+  databaseURL: "https://combiete-lantey-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const perf = getPerformance(app);
+const auth = getAuth(app);
