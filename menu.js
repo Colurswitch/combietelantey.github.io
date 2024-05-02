0e8ab1615d1954e0d5a1a18f53488110f5a73e5c
Colurswitch <celantey@gmail.com>
2024-05-02 19:54:28 -0400

    a

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-import data from "./menu.json" assert { type: 'json' };
+import data from "./menu.json" with { type: 'json' };
 
 const menu = document.querySelector("body > div > div > div.mdl-layout__drawer > nav");
 const menuItems = JSON.parse(data);
