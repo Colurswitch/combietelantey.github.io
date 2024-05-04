664f6e655ffd4938fb3cc8040b0816f4d328590c
Colurswitch <celantey@gmail.com>
2024-05-03 21:56:30 -0400

    s!!!

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-var firebase = require("firebase/compat/app");
+var {firebase} = require(["firebase/compat/app"]);
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
