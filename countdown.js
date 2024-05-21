95c7474f9920d74076da920e90216c4ad4d11909
Colurswitch <celantey@gmail.com>
2024-05-20 20:15:35 -0400

    a

---
+++ countdown.js
@@ -0,0 +1,30 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 25, 2024 16:00:00").getTime();
+// Set the HTML element the timer will display in
+var element = document.getElementById("countdown")
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  element.innerHTML = days + "days, " + hours + "hours, "
+  + minutes + "minutes and " + seconds + "seconds left";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    element.innerHTML = "EXPIRED";
+  }
+}, 1000);
