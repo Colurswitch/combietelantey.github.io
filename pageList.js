1de359e82f07c316a443108c80b5e577a5612077
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-02-11 21:09:54 -0500

    ...

--- pageList.js
+++ pageList.js
@@ -1,7 +1,7 @@
 const pageList = {
     generate: function(id,pageList) {
         document.getElementById(id).innerHTML = '';
-        pageList.forEach(element => {
+        pageList.forEach(page => {
             document.getElementById(id).innerHTML += `<div class="box" onclick="pageList.navigate('${page.destination}')">
                 <h1 class="page-icon material-icons">${page.icon}</h1>
                 <h3 class="page-title">${page.title}</h3>
