b6e6e76b86216283df8252377c630fa041f1b2d0
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-02-11 21:03:54 -0500

    yes

--- pageList.js
+++ pageList.js
@@ -1,13 +1,11 @@
 const pageList = {
     generate: function(id,pageList) {
         document.getElementById(id).innerHTML = '';
-        pageList.map( page => {
-            `<div class="box" onclick="pageList.navigate('${page.destination}')">
+        pageList.forEach(element => {
+            document.getElementById(id).innerHTML += `<div class="box" onclick="pageList.navigate('${page.destination}')">
                 <h1 class="page-icon material-icons">${page.icon}</h1>
                 <h3 class="page-title">${page.title}</h3>
-            </div>`
-        }).forEach(element => {
-            document.getElementById(id).innerHTML += element;
+            </div>`;
         });
     }
 }
