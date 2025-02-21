399f9227d87c3a373535f8ca1bd28c28d6758a3a
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-02-21 18:42:04 -0500

    fb

--- firebase.js
+++ firebase.js
@@ -25,11 +25,15 @@ const perf = getPerformance(app);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const user = auth.currentUser;
+var ui = new firebaseui.auth.AuthUI(auth);
 console.log(app);
 console.log(analytics);
 console.log(perf);
 console.log(auth);
 console.log(database);
+
+// Use FirebaseUI to handle the sign-in and sign-up.
+
 
 function signUp(email, password, stay) {
 	setPersistence(auth, stay? browserLocalPersistence : browserSessionPersistence);
@@ -59,5 +63,39 @@ function signIn(email, password, stay){
     const errorCode = error.code;
     const errorMessage = error.message;
 	  console.error(`${errorCode}: ${errorMessage}`);
+  });
+}
+
+function initFirebaseUI(){
+  ui.start('#firebaseui-auth-container', {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    //tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    //privacyPolicyUrl: '<your-privacy-policy-url>'
   });
 }
