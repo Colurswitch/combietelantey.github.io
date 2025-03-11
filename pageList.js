6ab209e279ea7ca07fd749f91fa8527d32cbf0ff
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-03-11 07:22:48 -0400

    Fixed games.html

--- pageList.js
+++ pageList.js
@@ -2,16 +2,15 @@ const pageList = {
     generate: function(id,pageList) {
         document.getElementById(id).innerHTML = '';
         pageList.forEach(page => {
-            document.getElementById(id).innerHTML += `<div class="box" onclick="pageList.navigate('${page.url}')">
+            document.getElementById(id).innerHTML += `<${page.external ? `a href="${page.url}"` : `div onclick="pageList.navigate('${page.url}')"`} class="box">
                 <h1 class="page-icon material-icons">${page.icon}</h1>
                 <h3 class="page-title">${page.title}</h3>
-            </div>`;
+            </${page.external ? "a" : "div"}>`;
         });
     },
     navigate: function(destination) {
         // Navigate to the specified destination page
         console.log(`Navigating to ${destination}`);
-        
         document.getElementById("inner-page-view").src = destination;
         document.getElementById("main-view").style.display = "none";
         document.getElementById("page-view").style.display = "flex";
