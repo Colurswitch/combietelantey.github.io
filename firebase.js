ce1b652f7db67d20790a89da4dc04f55bce30747
Colurswitch <71611550+Colurswitch@users.noreply.github.com>
2025-03-18 21:19:57 -0400

    Ugh

--- firebase.js
+++ firebase.js
@@ -204,7 +204,7 @@ const sbApp = {
   async getCurrentUserRecord() {
     // Check if user is signed in before trying to fetch their data.
     const current_user = await this.getCurrentUser();
-    if (!current_user.user) {
+    if (!current_user.data.user) {
       return { data: null, error: "User not signed in" };
     }
     const { data, error } = await supabase.from("users").select().eq(
