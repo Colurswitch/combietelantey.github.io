3e5abff72e186c1aa0467508d26343bf50f24898
ColurMine <celantey@gmail.com>
2025-06-26 12:03:18 -0400

    Batch commit: 1

--- games/static/2048/js/application.js
+++
@@ -1,5 +0,0 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-var gm;
-window.onload = function() {
-  gm = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-};
